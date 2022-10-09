2ac3d653e71894317e0ee1fee219da29bd6d6dc1
rindrajosia <rrindrajosia@gmail.com>
2022-10-09 16:59:20 +0300

    Create first server

---
+++ hello.js
@@ -0,0 +1,7 @@
+let http = require('http');
+
+http.createServer(function(req, res){
+	res.writeHead(200,{'Content-Type': 'text/plain'});
+	res.end('Hello world\n');
+}).listen(8124, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8124/');
